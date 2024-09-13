031f8a61afed910792ead367db125ad8ad7d45db
av7388605503 <av84002499@gmail.com>
2024-09-13 07:27:26 +0530

    update

--- src/App.js
+++ src/App.js
@@ -1,12 +1,10 @@
 import React from 'react'
 import Quiz from './components/Quiz'
-import Quizcreator from './components/Quizcreator'
 import "./App.css"
 function App() {
   return (
     <>
       <Quiz/>
-      <Quizcreator/>
     </>
   )
 }
